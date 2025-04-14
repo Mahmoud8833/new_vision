7039d57fd15d2cd620027e09d93db151be8326b5
mahmoud8833 <mahmoud.a.ali.88@gmail.com>
2025-04-14 23:30:43 +0200

    Add some testing code to index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Home</title>
+</head>
+
+<body>
+    <h1>Hello World - PHP</h1>
+
+    <?php
+    $name = "Mahmoud";
+    if ($name == "mahmoud") {
+        echo "<p>Yes</p>";
+        } else {
+        echo "<p>No</p>";
+        }
+    ?>
+
+</body>
+
+</html>
